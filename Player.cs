ca769ff30dc2666836238174cf09370207e6e37e
Alexandre <61777936+an363@users.noreply.github.com>
2025-04-09 16:38:21 +0200

    Add files via upload

--- Player.cs
+++ Player.cs
@@ -1,38 +1,42 @@
 // This is intended to be a public class. One public instance of it is stored in CarController, to give the specifics of the player (the ID, the test case, etc.)
 using System;
 using System.IO;
-
-public class Player
+using System.Globalization;
+using UnityEngine;
+public class Player: MonoBehaviour
 // constructs Player
 // selects Case
 // at the end of the run, call Player.ValidateRun()
 {
-	static string Folder_Cases= "/home/alexandre/Desktop/Lyon/Production/250115_VRoumAndGo/TEST/CASES";
-	static string File_Summary= "/home/alexandre/Desktop/Lyon/Production/250115_VRoumAndGo/TEST/summary.txt";
-
-	static int seed=10; // not necessary -- use the random generator of the game
-	static Random Rand = new Random(seed);
+	//static int seed=10; // not necessary -- use the random generator of the game
+	static System.Random Rand = new System.Random();
 
 	
 	public int myID=-1; // ID of the player
 	public int mycase; // scenario in which they are playing (noise, delay, etc.) --> call getCaseLabel() to get the letter ("A", "B", "C", etc.)
 	public int myseries=-1; // series number within that scenario
 	public int last_ID=-1; // ID of the car in front; this is 0 if no other player has played in this case/series yet 
+	public string Folder_Cases;
+	public string File_Summary;
+	public string File_saving;
+	
+	public float gap=-1.0f; // car_length + gap is the inverse density
+	public float delay=-1.0f;
+	public float noise=-1.0f;
 	
 		
-	static void Main()
-	{
-	Player player= new Player();
-	player.ValidateRun();	
-	
-	}
-	
-	public Player()
+	void Start()
 	// this initializes the player, with their ID and attributes
 	{
-		
+		Folder_Cases= Path.Combine(Application.persistentDataPath, "CASES");
+		File_Summary= Path.Combine(Application.persistentDataPath, "summary.txt");
 		this.myID = (int) DateTimeOffset.UtcNow.Subtract(new DateTime(2025, 1, 1)).TotalSeconds;
+		File_saving= Path.Combine(Application.persistentDataPath, "DATA", this.myID + ".txt");
 		
+		//Debug.Log("Player ID: " + this.myID);
+		//Debug.Log("Folder: " + Folder_Cases);
+		//Debug.Log("Summary: " + File_Summary);
+
 		this.SelectCase();
 		
 		// check if myFile.txt file is created at the specified path 
@@ -45,7 +49,6 @@ public class Player
 			File.Create(File_Summary);
 			Console.WriteLine("File was not created.");
 		}
-		
 	}
 	
 
@@ -95,7 +98,7 @@ public class Player
 	}
 
 
-	this.mycase= (Rand.Next() % nb_cases); // randomly selected case
+	this.mycase= Rand.Next() % nb_cases; // randomly selected case
 	
 	int series_selected= 0;
 	while(nb_drivers[this.mycase,series_selected]>50) // assume 50 runs are needed
@@ -113,12 +116,40 @@ public class Player
 		return (char) ((int) 'A' + this.mycase);
 	}
 	
-	public void ValidateRun()
+	public void ReadParameters()
 	{
-		if (true) // if the run was valid
-			File.AppendAllText(File_Summary,string.Format("{0} {1} {2} yes", this.myID,this.getCaseLabel(),this.myseries) );
+	string CasePath= Path.Combine(Folder_Cases,  Char.ToString(getCaseLabel()) + ".txt");
+	string  text= File.ReadAllText (CasePath);
+	string[] lines = text.Split(new string[] { "\n" }, StringSplitOptions.None);
+	
+		try
+		{
+		string[] fields = lines[1].Split(new string[] { "=" }, StringSplitOptions.None);
+		gap= float.Parse(fields[1], CultureInfo.InvariantCulture);
+		fields = lines[2].Split(new string[] { "=" }, StringSplitOptions.None);
+		delay= float.Parse(fields[1], CultureInfo.InvariantCulture);
+		fields = lines[3].Split(new string[] { "=" }, StringSplitOptions.None);
+		noise= float.Parse(fields[1], CultureInfo.InvariantCulture);
+		}
+		catch (IOException e)
+		{
+		    Console.WriteLine("The config file " + Char.ToString(getCaseLabel()) +".txt could not be read.");
+		    Console.WriteLine(e.Message);
+		}		
+			
+	}
+	
+	public void ValidateRun(float ratioTooClose, float ratioTooFar)
+	{
+		//if (true) // if the run was valid
+		if ((ratioTooClose < 0.5) && (ratioTooFar < 0.5) && (ratioTooClose + ratioTooFar < 0.75))
+		{
+			File.AppendAllText(File_Summary,string.Format("{0} {1} {2} yes \n", this.myID,this.getCaseLabel(),this.myseries));
+		}
 		else
+		{
 			File.AppendAllText(File_Summary,string.Format("{0} {1} {2} false", this.myID,this.getCaseLabel(),this.myseries) );
+		}
 	}
 }
 
