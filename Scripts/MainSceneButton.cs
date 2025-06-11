eac178da8de23fc8484b8f1b9a5cf68765ecbf08
Enock Ndunda <115922042+enockn@users.noreply.github.com>
2025-06-11 17:43:32 +0200

    Update MainSceneButton.cs
    I have also updated the main scene buttons to accommodate the new changes in StartScene and CarInFront.

--- Scripts/MainSceneButton.cs
+++ Scripts/MainSceneButton.cs
@@ -2,29 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
+using System.Collections;
+using TMPro;
  
  
 public class GameMenu : MonoBehaviour
 
 {
+ public TextMeshProUGUI PlayAgain;
 	public static bool isPaused;
 
 	public void PauseGame()
 		{
 		Time.timeScale = 0f;
 		isPaused = true;
+		PlayAgain.text = "'Restart' to play again.";
 		}
 
 	public void ResumeGame()
 		{
 		Time.timeScale = 1f;
 		isPaused = false;
+		
 		}
 
 	public void GoToStartMenue()
 		{
 		Time.timeScale = 1f;
+		isPaused = false;
+		PlayAgain.text = "";
 		SceneManager.LoadScene("StartScene");
 		}
 		
